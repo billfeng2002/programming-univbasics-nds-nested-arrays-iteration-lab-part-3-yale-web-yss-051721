d6bb1989f5c09313d51b246f63df68929be44313
Bill Feng <bill.feng@yale.edu>
2021-05-24 07:26:19 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   for array in src do
     for value in array do
       if(value.class == String)
-        puts "caught"
+        #puts "caught"
         newstr+=value
       end
     end
@@ -15,4 +15,4 @@ def join_nested_strings(src)
   newstr
 end
 
-p join_nested_strings([["adfdf", 2], [4,"2wldef"]])
+#p join_nested_strings([["adfdf", 2], [4,"2wldef"]])
